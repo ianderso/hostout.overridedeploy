f8ea12ab3db2825208d63b83630a97bac4704301
Ian Anderson <ian@andersonleeb.com>
2012-05-05 02:23:25 +0000

    added part to deal with empty pinned file

--- hostout/overridedeploy/fabfile.py
+++ hostout/overridedeploy/fabfile.py
@@ -1,11 +1,13 @@
 from fabric import api
-
+import tempfile
+from collective.hostout.hostout import buildoutuser
 
 def deploy():
     "predeploy, uploadeggs, uploadbuildout, buildout and then postdeploy"
 
     hostout = api.env['hostout']
     hostout.predeploy()
+    hostout.uploadeggs()
     hostout.uploadbuildout()
     hostout.buildout()
     hostout.postdeploy()
@@ -19,3 +21,18 @@ def predeploy():
     f.close()
 
     api.env.superfun()
+
+@buildoutuser
+def uploadeggs():
+    """Release developer eggs and send to host """
+
+    hostout = api.env['hostout']
+
+    # Ensure there is no local pinned.cfg so we don't clobber it
+    # Now upload pinned.cfg.
+    pinned = "[buildout]\ndevelop=\n[versions]\n"
+    tmp = tempfile.NamedTemporaryFile()
+    tmp.write(pinned)
+    tmp.flush()
+    api.put(tmp.name, api.env.path+'/pinned.cfg')
+    tmp.close()
